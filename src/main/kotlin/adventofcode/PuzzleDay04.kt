d6c918470f94356ae7bfe73bcba0863118eade46
Marius Schmidt <marius@denktmit.de>
2021-12-06 22:34:02 +0100

    Solved Day 4 Part 1

--- src/main/kotlin/adventofcode/PuzzleDay04.kt
+++ src/main/kotlin/adventofcode/PuzzleDay04.kt
@@ -1,4 +1,71 @@
 package adventofcode
 
+import java.io.File
+
 class PuzzleDay04 {
+
+
+    fun determineFirstWinningBoard(file: File): BingoBoard {
+        val pulls = readPulls(file)
+        val boards = readBoards(file)
+        val first = pulls.flatMap { pulledNumber ->
+                boards.onEach { it.drawNumber(pulledNumber) }.filter { it.won }
+            }.first()
+        return first
+    }
+
+    private fun readPulls(file: File): List<Int> = file.useLines { seq ->
+        seq.first().split(",").map { it.toInt() }
+    }
+
+    private fun readBoards(file: File): List<BingoBoard> = file.useLines { seq ->
+        seq.drop(2)
+            .flatMap { it.split(" ") }
+            .filter { it.isNotEmpty() }
+            .map { it.toInt() }
+            .windowed(25, 25)
+            .mapIndexed { index, boardNumbers -> BingoBoard(index + 1, boardNumbers.toIntArray()) }
+            .toList()
+    }
+
+
+    class BingoBoard(val id: Int, numbers: IntArray) {
+
+        companion object {
+            const val X_SIZE = 5
+            const val Y_SIZE = 5
+            const val WINNING_COUNT = 5
+        }
+
+        private val fields: Array<BingoBoardField> = numbers.map { BingoBoardField(it) }.toTypedArray()
+        var won: Boolean = false
+        var drawsToWin: Int = 0
+        var lastNumber: Int = -1
+
+        fun score(): Int = if (!won) 0 else fields.filter { !it.pulled }.sumOf { it.number } * lastNumber
+
+        fun drawNumber(number: Int) {
+            if (won) return
+            fields.filter { it.number == number }.onEach { it.pulled = true }
+            drawsToWin++
+            won = checkWon()
+            lastNumber = number
+        }
+
+        private fun checkWon(): Boolean =
+            ((0 until X_SIZE).map { columnFields(it) } + (0 until Y_SIZE).map { rowFields(it) }).any { it.checkWon() }
+
+        private fun List<BingoBoardField>.checkWon(): Boolean = count { it.pulled } >= WINNING_COUNT
+
+        private fun rowFields(rowIndex: Int): List<BingoBoardField> =
+            fields.slice(rowIndex * X_SIZE until rowIndex * X_SIZE + X_SIZE)
+
+        private fun columnFields(columnIndex: Int): List<BingoBoardField> =
+            fields.slice(columnIndex until X_SIZE * Y_SIZE step X_SIZE)
+
+    }
+
+    class BingoBoardField(val number: Int) {
+        var pulled: Boolean = false
+    }
 }
--- src/test/kotlin/adventofcode/PuzzleDay04Test.kt
+++ src/test/kotlin/adventofcode/PuzzleDay04Test.kt
@@ -1,9 +1,8 @@
+
 package adventofcode
 
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import java.io.File
+import org.junit.jupiter.api.Test
 
 internal class PuzzleDay04Test {
 
@@ -11,5 +10,52 @@ internal class PuzzleDay04Test {
     private val testfile = FileHelper.testfile(day)
     private val datafile = FileHelper.datafile(day)
 
+    @Test
+    internal fun testBingoGameNotWon() {
+        val bingo = PuzzleDay04.BingoBoard(1, IntArray(25) { i -> i + 1})
+        (1 .. 25 step 2).forEach { bingo.drawNumber(it) }
+        assertFalse(bingo.won)
+    }
 
+    @Test
+    internal fun testBingoGameWonInLastColumn() {
+        val bingo = PuzzleDay04.BingoBoard(1, IntArray(25) { i -> i + 1})
+        bingo.drawNumber(1)
+        ( 5 .. 25 step 5).forEach {
+            bingo.drawNumber(it)
+        }
+        bingo.drawNumber(2)
+        assertTrue(bingo.won)
+        assertEquals(6, bingo.drawsToWin)
+        assertEquals(25, bingo.lastNumber)
+        assertEquals(6175, bingo.score())
+    }
+
+    @Test
+    internal fun testBingoGameWonInLastRow() {
+        val bingo = PuzzleDay04.BingoBoard(1, IntArray(25) { i -> i + 1})
+        bingo.drawNumber(1)
+        bingo.drawNumber(2)
+        (21 .. 25).forEach {
+            bingo.drawNumber(it)
+        }
+        assertTrue(bingo.won)
+        assertEquals(7, bingo.drawsToWin)
+        assertEquals(25, bingo.lastNumber)
+        assertEquals(5175, bingo.score())
+    }
+
+    @Test
+    internal fun determineFirstWinningBoardSample() {
+        val firstWinningBoard = PuzzleDay04().determineFirstWinningBoard(testfile)
+        assertEquals(3, firstWinningBoard.id)
+        assertEquals(4512, firstWinningBoard.score())
+    }
+
+    @Test
+    internal fun determineFirstWinningBoard() {
+        val firstWinningBoard = PuzzleDay04().determineFirstWinningBoard(datafile)
+        assertEquals(44, firstWinningBoard.id)
+        assertEquals(58374, firstWinningBoard.score())
+    }
 }
